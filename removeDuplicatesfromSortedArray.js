2f8adfc27a024af18bad81ba0eb2ab7c46bac7f2
jmalone801 <jmalone801@gmail.com>
2022-02-22 15:09:09 -0500

    Algo

---
+++ removeDuplicatesfromSortedArray.js
@@ -0,0 +1,17 @@
+// Given an integer array nums sorted in non-decreasing order, 
+//remove the duplicates in-place such that each unique element appears only once. 
+//The relative order of the elements should be kept the same.
+
+/*
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === nums[i + 1]) {
+            nums.splice(i, 1)
+            i--
+        }
+    }
+    return nums.length
+};
