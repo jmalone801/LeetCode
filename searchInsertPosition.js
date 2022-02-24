07f13db1bb45e0a3d7c9f94f603ce389aec4a198
jmalone801 <jmalone801@gmail.com>
2022-02-24 18:28:50 -0500

    Algo

---
+++ searchInsertPosition.js
@@ -0,0 +1,18 @@
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. 
+// If not, return the index where it would be if it were inserted in order.
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === target || nums[i] > target) {
+            return i
+        }
+        if (i === nums.length - 1) {
+            return i + 1
+        }
+    }
+};
