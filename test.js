65637e6cbae69f16f5fbfdf79d854598f219d71a
jmalone801 <jmalone801@gmail.com>
2022-11-25 16:29:41 -0500

    Test

--- test.js
+++ test.js
@@ -1,11 +1,2 @@
-function Compare(str1, str2) {
-    if (str1.length !== str2.length) {
-        return false
-    }
-
-    return str1.split("").sort().join("") === str2.split("").sort().join("")
-}
-
-console.log(Compare("listen", "silent")) //true
-console.log(Compare("mary", "arms")) //false
-console.log(Compare("aabbc", "aabbc")) //true
+var a = "me";
+console.log(a);
